8c233b7243c8b98f967d3bddca23aa77933f57f3
Johnson Keiriz <jkeiriz@tamaggo.com>
2015-11-30 16:04:32 -0500

    created gun as a member of the particle generator

--- my_tests/SimpleDet/src/BasicPrimaryGeneratorAction.cc
+++ my_tests/SimpleDet/src/BasicPrimaryGeneratorAction.cc
@@ -1,25 +1,43 @@
 #include "BasicPrimaryGeneratorAction.hh"
 
+BasicPrimaryGeneratorAction::BasicPrimaryGeneratorAction()
+	: G4VUserPrimaryGeneratorAction(),
+	  fParticleGun(0)
+{
+	G4int n_particle = 1;
+	fParticleGun  = new G4ParticleGun(n_particle);
+
+	// default particle kinematic
+	G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+	G4String particleName;
+	G4ParticleDefinition* particle
+			= particleTable->FindParticle(particleName="gamma");
+	fParticleGun->SetParticleDefinition(particle);
+	fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
+	fParticleGun->SetParticleEnergy(6.*MeV);
+}
+
+BasicPrimaryGeneratorAction::~BasicPrimaryGeneratorAction()
+{
+  delete fParticleGun;
+}
+
+
 void BasicPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
-	//Create particle gun
-	G4ParticleGun* myGun = new G4ParticleGun();
 
+	/*
 	//Specify particle to be emitted
-	myGun->SetParticleDefinition(G4Gamma::GammaDefinition());
-	myGun->SetParticlePosition(G4ThreeVector(0.,0.,-70.*cm));
-	//myGun->SetNumberOfParticles();
-	//Set particle  kinetic energy and direction of travel
-	myGun->SetParticleEnergy(6.*MeV);
-	// travelling in the x axis
-	myGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1));
+	// gun is focused on detector evenly flooding it
+	// used for flooded image method
+	G4double size = 0.8;
+	G4double x0 = size * 15. * (G4UniformRand()-0.5);
+	G4double y0 = size * 15. * (G4UniformRand()-0.5);
+	fParticleGun->SetParticlePosition(G4ThreeVector(x0*cm, y0*cm, -70.*cm));
+	*/
+	// for the LSF method depending on PSF
+	fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., -70.*cm));
 
-	//myGun->SetParticleTime();
-
-	// for a point source
-	myGun->SetParticleMomentum(G4RandomDirection());
-
-	//myGun->SetNumberOfParticles();
 	//Generate one instance of specified particle
-	myGun->GeneratePrimaryVertex(anEvent);
+	fParticleGun->GeneratePrimaryVertex(anEvent);
 }
--- my_tests/SimpleDet/src/BasicPrimaryGeneratorAction.hh
+++ my_tests/SimpleDet/src/BasicPrimaryGeneratorAction.hh
@@ -3,15 +3,25 @@
 
 #include "G4VUserPrimaryGeneratorAction.hh"
 #include "G4ParticleGun.hh"
-#include "G4Proton.hh"
 #include "G4Gamma.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4RandomDirection.hh"
 #include "G4GeneralParticleSource.hh"
+#include "globals.hh"
+#include "G4ParticleTable.hh"
 
-class BasicPrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction{
+class G4ParticleGun;
+
+class BasicPrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
+{
+
 public:
+	BasicPrimaryGeneratorAction();
+	virtual ~BasicPrimaryGeneratorAction();
+
 	void GeneratePrimaries(G4Event* anEvent);
+private:
+	G4ParticleGun*  fParticleGun; // pointer to a G4 gun class
 };
 
 #endif
