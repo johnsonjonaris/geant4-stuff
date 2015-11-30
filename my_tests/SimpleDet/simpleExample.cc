4a1b6ec99a76646e792a7d8e7ede6d1bfb3dbba8
Johnson Keiriz <jkeiriz@tamaggo.com>
2015-11-30 11:52:01 -0500

    removing enveloppe, adding Compton penelope model

--- my_tests/SimpleDet/simpleExample.cc
+++ my_tests/SimpleDet/simpleExample.cc
@@ -13,6 +13,7 @@
 #include "G4ComptonScattering.hh"
 #include "G4GammaConversion.hh"
 #include "G4Scintillation.hh"
+#include "G4PenelopeComptonModel.hh"
 
 #include "G4UImanager.hh"
 #include "G4VisExecutive.hh"
@@ -20,95 +21,99 @@
 
 class MyPhysicsList : G4VUserPhysicsList
 {
-    void ConstructEM();
+	void ConstructEM();
 public:
-    void ConstructProcess();
+	void ConstructProcess();
 };
 
 void MyPhysicsList::ConstructProcess()
 {
-    // Define transportation process
-    AddTransportation();
-    // electromagnetic processes
-    ConstructEM();
+	// Define transportation process
+	AddTransportation();
+	// electromagnetic processes
+	ConstructEM();
 }
 
 void MyPhysicsList::ConstructEM()
 {
-    // Get pointer to G4PhysicsListHelper
-    G4PhysicsListHelper* ph = G4PhysicsListHelper::GetPhysicsListHelper();
+	// Get pointer to G4PhysicsListHelper
+	G4PhysicsListHelper* ph = G4PhysicsListHelper::GetPhysicsListHelper();
 
-    //  Get pointer to gamma
-    G4ParticleDefinition* particle = G4Gamma::GammaDefinition();
+	//  Get pointer to gamma
+	G4ParticleDefinition* particle = G4Gamma::GammaDefinition();
 
-    // Construct and register processes for gamma
-    // control scintillation factor to 400 Photon/MeV
-    G4double beta = 400;
-    G4Scintillation *scin = new G4Scintillation();
-    scin->SetScintillationYieldFactor(beta);
-    ph->RegisterProcess(scin, particle);
-    ph->RegisterProcess(new G4PhotoElectricEffect(), particle);
-    ph->RegisterProcess(new G4ComptonScattering(), particle);
-    ph->RegisterProcess(new G4GammaConversion(), particle);
+	// Construct and register processes for gamma
+	// control scintillation factor to 400 Photon/MeV
+	/*
+	G4double beta = 400;
+	G4Scintillation *scin = new G4Scintillation();
+	scin->SetScintillationYieldFactor(beta);
+	ph->RegisterProcess(scin, particle);
+	*/
+	ph->RegisterProcess(new G4PhotoElectricEffect(), particle);
+	G4ComptonScattering* theComptonScattering = new G4ComptonScattering();
+	theComptonScattering->SetModel(new G4PenelopeComptonModel());
+	ph->RegisterProcess(theComptonScattering, particle);
+	ph->RegisterProcess(new G4GammaConversion(), particle);
 }
 
 int main(int argc,char** argv){
 
-    // Detect interactive mode (if no arguments) and define UI session
-    //
-    G4UIExecutive* ui = 0;
-    if ( argc == 1 ) {
-        ui = new G4UIExecutive(argc, argv);
-    }
+	// Detect interactive mode (if no arguments) and define UI session
+	//
+	G4UIExecutive* ui = 0;
+	if ( argc == 1 ) {
+		ui = new G4UIExecutive(argc, argv);
+	}
 
-    G4RunManager * runManager = new G4RunManager;
-    // Detector construction
-    runManager->SetUserInitialization(new BasicDetectorConstruction());
+	G4RunManager * runManager = new G4RunManager;
+	// Detector construction
+	runManager->SetUserInitialization(new BasicDetectorConstruction());
 
-    // Physics list
-    // the physics list must always be registered before the primary generator action can
-    // be instantiated since otherwise the relevant particle definitions may not be available.
-    G4VModularPhysicsList* physicsList = new QBBC;
-    runManager->SetUserInitialization(physicsList);
+	// Physics list
+	// the physics list must always be registered before the primary generator action can
+	// be instantiated since otherwise the relevant particle definitions may not be available.
+	G4VModularPhysicsList* physicsList = new QBBC;
+	runManager->SetUserInitialization(physicsList);
 
-    // Primary generator action
-    runManager->SetUserAction(new BasicPrimaryGeneratorAction());
+	// Primary generator action
+	runManager->SetUserAction(new BasicPrimaryGeneratorAction());
 
-    // Initialize G4 kernel
-    runManager->Initialize();
+	// Initialize G4 kernel
+	runManager->Initialize();
 
-    // Initialize visualization
-    //
-    G4VisManager* visManager = new G4VisExecutive;
-    // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
-    // G4VisManager* visManager = new G4VisExecutive("Quiet");
-    visManager->Initialize();
+	// Initialize visualization
+	//
+	G4VisManager* visManager = new G4VisExecutive;
+	// G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+	// G4VisManager* visManager = new G4VisExecutive("Quiet");
+	visManager->Initialize();
 
-    // Get the pointer to the User Interface manager
-    G4UImanager* UImanager = G4UImanager::GetUIpointer();
+	// Get the pointer to the User Interface manager
+	G4UImanager* UImanager = G4UImanager::GetUIpointer();
 
-    //Cause the run manager to generate a single event using the
-    //primary generator action registered above.
-    // runManager->BeamOn(1);
+	//Cause the run manager to generate a single event using the
+	//primary generator action registered above.
+	// runManager->BeamOn(1);
 
-    // Process macro or start UI session
-    //
-    if ( ! ui ) {
-        // batch mode
-        G4String command = "/control/execute ";
-        G4String fileName = argv[1];
-        UImanager->ApplyCommand(command+fileName);
-    }
-    else {
-        // interactive mode
-        UImanager->ApplyCommand("/control/execute init_vis.mac");
-        ui->SessionStart();
-        delete ui;
-    }
+	// Process macro or start UI session
+	//
+	if ( ! ui ) {
+		// batch mode
+		G4String command = "/control/execute ";
+		G4String fileName = argv[1];
+		UImanager->ApplyCommand(command+fileName);
+	}
+	else {
+		// interactive mode
+		UImanager->ApplyCommand("/control/execute init_vis.mac");
+		ui->SessionStart();
+		delete ui;
+	}
 
-    //After the run is complete, free up the memory used by run
-    //manager and return
-    delete visManager;
-    delete runManager;
-    return 0;
+	//After the run is complete, free up the memory used by run
+	//manager and return
+	delete visManager;
+	delete runManager;
+	return 0;
 }
--- my_tests/SimpleDet/src/DetectorConstruction.cc
+++ my_tests/SimpleDet/src/DetectorConstruction.cc
@@ -9,83 +9,59 @@ class BasicDetectorConstruction : public G4VUserDetectorConstruction
 {
 public: 
 
-    G4VPhysicalVolume* Construct()
-    {
-        //Obtain pointer to NIST material manager
-        G4NistManager* nist = G4NistManager::Instance();
+	G4VPhysicalVolume* Construct()
+	{
+		//Obtain pointer to NIST material manager
+		G4NistManager* nist = G4NistManager::Instance();
 
-        //Build materials for world and target
-        G4Material* CsI = G4Material::GetMaterial("G4_CESIUM_IODIDE");
-        G4Material* air = nist->FindOrBuildMaterial("G4_AIR");
+		//Build materials for world and target
+		G4Material* CsI = nist->FindOrBuildMaterial("G4_CESIUM_FLUORIDE");
+		G4Material* air = nist->FindOrBuildMaterial("G4_AIR");
 
-        //Define the world solid. The world will be 5x5x5 m3
-        G4double env_sizeXY = 2.5*m;
-        G4double env_sizeZ = 3.*m;
-        G4double world_sizeXY = 1.2*env_sizeXY;
-        G4double world_sizeZ  = 1.2*env_sizeZ;
-        G4Box* solidWorld =
-                new G4Box("World",world_sizeXY,world_sizeXY,world_sizeZ);
+		//Define the world solid
+		G4double world_sizeXY = 1.2*m;
+		G4double world_sizeZ  = 2.*m;
 
-        //Fill the world with air
-        G4LogicalVolume* logicWorld =
-                new G4LogicalVolume(solidWorld, air, "World");
+		G4Box* solidWorld =
+				new G4Box("World",world_sizeXY,world_sizeXY,world_sizeZ);
+		//Fill the world with air
+		G4LogicalVolume* logicWorld =
+				new G4LogicalVolume(solidWorld, air, "World");
+		//Create the world physical volume. The world is the only
+		//physical volume with no mother volume.
+		G4VPhysicalVolume* physWorld =
+				new G4PVPlacement(0,                       //no rotation
+								  G4ThreeVector(),       //at (0,0,0)
+								  logicWorld,            //its logical volume
+								  "World",               //its name
+								  0,                     //its mother  volume
+								  false,                 //no boolean operation
+								  0,                     //copy number
+								  true);			       //overlaps checking
 
-        //
-        // Envelope
-        //
-        G4Box* solidEnv =
-          new G4Box("Envelope",                    //its name
-              0.5*env_sizeXY, 0.5*env_sizeXY, 0.5*env_sizeZ); //its size
+		//Create  the shape of a target to fire particles at
+		G4double target_sizeX=15.*cm;
+		G4double target_sizeY=15.*cm;
+		G4double target_sizeZ=40.*mm;
+		G4Box* solidDetector =
+				new G4Box("Detector",target_sizeX, target_sizeY, target_sizeZ);
 
-        G4LogicalVolume* logicEnv =
-          new G4LogicalVolume(solidEnv,            //its solid
-                              air,             //its material
-                              "Envelope");         //its name
+		//Create the target logical volume by
+		//assigning the material of the target to be Pb
+		G4LogicalVolume* logicDetector =
+				new G4LogicalVolume(solidDetector, CsI, "Detector");
 
-        new G4PVPlacement(0,                       //no rotation
-                          G4ThreeVector(),         //at (0,0,0)
-                          logicEnv,                //its logical volume
-                          "Envelope",              //its name
-                          logicWorld,              //its mother  volume
-                          false,                   //no boolean operation
-                          0,                       //copy number
-                          true);          //overlaps checking
-
-        //Create the world physical volume. The world is the only
-        //physical volume with no mother volume.
-        G4VPhysicalVolume* physWorld =
-                new G4PVPlacement(0,                       //no rotation
-                                  G4ThreeVector(),       //at (0,0,0)
-                                  logicWorld,            //its logical volume
-                                  "World",               //its name
-                                  0,                     //its mother  volume
-                                  false,                 //no boolean operation
-                                  0,                     //copy number
-                                  true);			       //overlaps checking
-
-        //Create  the shape of a target to fire particles at
-        G4double target_sizeX=15.*cm;
-        G4double target_sizeY=15.*cm;
-        G4double target_sizeZ=40.*mm;
-        G4Box* solidDetector =
-                new G4Box("Target",target_sizeX, target_sizeY, target_sizeZ);
-
-        //Create the target logical volume by
-        //assigning the material of the target to be Pb
-        G4LogicalVolume* logicDetector =
-                new G4LogicalVolume(solidDetector, CsI, "myTarget");
-
-        //Create the target physical volume by placing it in the
-        //"logicWorld" logical volume.
-        G4VPhysicalVolume* physDetector =
-                new G4PVPlacement(0,                       //no rotation
-                                  G4ThreeVector(0.,0.,70.*cm),
-                                  logicDetector,           //its logical volume
-                                  "World",               //its name
-                                  logicWorld,             //its mother  volume
-                                  false,                 //no boolean operation
-                                  0,                     //copy number
-                                  true);			       //overlaps checking
-        return physWorld;
-    }
+		//Create the target physical volume by placing it in the
+		//"logicWorld" logical volume.
+		G4VPhysicalVolume* physDetector =
+				new G4PVPlacement(0,                       //no rotation
+								  G4ThreeVector(0.,0.,70.*cm),
+								  logicDetector,           //its logical volume
+								  "World",               //its name
+								  logicWorld,             //its mother  volume
+								  false,                 //no boolean operation
+								  0,                     //copy number
+								  true);			       //overlaps checking
+		return physWorld;
+	}
 };
