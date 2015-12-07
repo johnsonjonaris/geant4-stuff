f5dc61aed18b6393c6070b3189fa9a7f29fbab35
Johnson Keiriz <jkeiriz@tamaggo.com>
2015-12-07 13:00:42 -0500

    modified physics list according to optical examples: OpNovice and LXe

--- my_tests/SimpleDet/src/PhysicsList.cc
+++ my_tests/SimpleDet/src/PhysicsList.cc
@@ -1,40 +1,223 @@
 #include "PhysicsList.hh"
-#include "G4SystemOfUnits.hh"
+
 void MyPhysicsList::ConstructProcess()
 {
 	// Define transportation process
 	AddTransportation();
+	ConstructDecay();
 	// electromagnetic processes
 	ConstructEM();
-	// according to paper, set cut value
-	G4double cutValue = 0.5*um;
-	SetDefaultCutValue(cutValue);
+	// Optical Physics
+	ConstructOp();
 }
 
 void MyPhysicsList::ConstructEM()
 {
-	// Get pointer to G4PhysicsListHelper
-	G4PhysicsListHelper* ph = G4PhysicsListHelper::GetPhysicsListHelper();
+	theParticleIterator->reset();
+	while( (*theParticleIterator)() ){
+		G4ParticleDefinition* particle = theParticleIterator->value();
+		G4ProcessManager* pmanager = particle->GetProcessManager();
+		G4String particleName = particle->GetParticleName();
 
-	//  Get pointer to gamma
-	G4ParticleDefinition* particle = G4Gamma::GammaDefinition();
+		if (particleName == "gamma") {
+			// gamma
+			// Construct processes for gamma
+			G4GammaConversion* fPairProduction = new G4GammaConversion();
+			pmanager->AddDiscreteProcess(fPairProduction);
+			G4ComptonScattering* theComptonScattering = new G4ComptonScattering();
+			theComptonScattering->SetEmModel(new G4PenelopeComptonModel());
+			pmanager->AddDiscreteProcess(theComptonScattering);
+			G4PhotoElectricEffect* thePhotoElectricEffect = new G4PhotoElectricEffect();
+			thePhotoElectricEffect->SetEmModel(new G4PenelopePhotoElectricModel());
+			pmanager->AddDiscreteProcess(thePhotoElectricEffect);
 
-	// Construct and register processes for gamma
-	// control scintillation factor to 400 Photon/MeV
-	/*
-	G4double beta = 400;
-	G4Scintillation *scin = new G4Scintillation();
-	scin->SetScintillationYieldFactor(beta);
-	ph->RegisterProcess(scin, particle);
-	*/
-	G4PhotoElectricEffect* thePhotoElectricEffect = new G4PhotoElectricEffect();
-	thePhotoElectricEffect->SetEmModel(new G4PenelopePhotoElectricModel());
-	ph->RegisterProcess(thePhotoElectricEffect, particle);
-	G4ComptonScattering* theComptonScattering = new G4ComptonScattering();
-	theComptonScattering->SetEmModel(new G4PenelopeComptonModel());
-	ph->RegisterProcess(theComptonScattering, particle);
-	ph->RegisterProcess(new G4GammaConversion(), particle);
-	G4RayleighScattering* theRayleighScattering = new G4RayleighScattering();
-	theRayleighScattering->SetEmModel(new G4PenelopeRayleighModel());
-	ph->RegisterProcess(theRayleighScattering, particle);
+		} else if (particleName == "e-") {
+			//electron
+			// Construct processes for electron
+			pmanager->AddProcess(new G4eMultipleScattering(),-1, 1, 1);
+			pmanager->AddProcess(new G4eIonisation(),       -1, 2, 2);
+			pmanager->AddProcess(new G4eBremsstrahlung(),   -1, 3, 3);
+
+		} else if (particleName == "e+") {
+			//positron
+			// Construct processes for positron
+			pmanager->AddProcess(new G4eMultipleScattering(),-1, 1, 1);
+			pmanager->AddProcess(new G4eIonisation(),       -1, 2, 2);
+			pmanager->AddProcess(new G4eBremsstrahlung(),   -1, 3, 3);
+			pmanager->AddProcess(new G4eplusAnnihilation(),  0,-1, 4);
+
+		} else if ( particleName == "mu+" ||
+					particleName == "mu-"    ) {
+			//muon
+			// Construct processes for muon
+			pmanager->AddProcess(new G4MuMultipleScattering(),-1, 1, 1);
+			pmanager->AddProcess(new G4MuIonisation(),      -1, 2, 2);
+			pmanager->AddProcess(new G4MuBremsstrahlung(),  -1, 3, 3);
+			pmanager->AddProcess(new G4MuPairProduction(),  -1, 4, 4);
+			if(particleName == "mu-") {
+				pmanager->AddRestProcess(new G4MuonMinusCapture());
+			}
+
+		} else {
+			if ((particle->GetPDGCharge() != 0.0) &&
+					(particle->GetParticleName() != "chargedgeantino") &&
+					!particle->IsShortLived()) {
+				// all others charged particles except geantino
+				pmanager->AddProcess(new G4hMultipleScattering(),-1,1,1);
+				pmanager->AddProcess(new G4hIonisation(),       -1,2,2);
+			}
+		}
+	}
 }
+
+void MyPhysicsList::ConstructParticle()
+{
+	// In this method, static member functions should be called
+	// for all particles which you want to use.
+	// This ensures that objects of these particle types will be
+	// created in the program.
+
+	G4BosonConstructor bConstructor;
+	bConstructor.ConstructParticle();
+	/* Equivalent to:
+	// gamma
+	G4Gamma::GammaDefinition();
+
+	// pseudo-particles
+	// https://geant4.web.cern.ch/geant4/UserDocumentation/UsersGuides/ForApplicationDeveloper/html/ch05s03.html
+	// virtual particles for simulation which do not interact with materials and undertake transportation processes only.
+	G4Geantino::GeantinoDefinition();
+	G4ChargedGeantino::ChargedGeantinoDefinition();
+
+	// optical photon
+	G4OpticalPhoton::OpticalPhotonDefinition();
+	*/
+	G4LeptonConstructor lConstructor;
+	lConstructor.ConstructParticle();
+	/* Equivalent to:
+	// electron
+	G4Electron::ElectronDefinition();
+	G4Positron::PositronDefinition();
+	G4NeutrinoE::NeutrinoEDefinition();
+	G4AntiNeutrinoE::AntiNeutrinoEDefinition();
+
+	// Mu
+	G4MuonPlus::MuonPlusDefinition();
+	G4MuonMinus::MuonMinusDefinition();
+	G4NeutrinoMu::NeutrinoMuDefinition();
+	G4AntiNeutrinoMu::AntiNeutrinoMuDefinition();
+	*/
+
+	G4MesonConstructor mConstructor;
+	mConstructor.ConstructParticle();
+	/* Equivalent to:
+	//These are needed for the mu- capture
+	G4PionMinus::PionMinus();
+	G4PionZero::PionZero();
+	G4PionPlus::PionPlus();
+	 and much more
+	*/
+
+	G4BaryonConstructor rConstructor;
+	rConstructor.ConstructParticle();
+	/* Equivalent to:
+	//These are needed for the mu- capture
+	G4Neutron::Neutron();
+	G4Proton::Proton();
+	*/
+
+	G4IonConstructor iConstructor;
+	iConstructor.ConstructParticle();
+	// Equivalent to:
+	// G4GenericIon::GenericIonDefinition();
+}
+
+void MyPhysicsList::ConstructDecay()
+{
+	// Add Decay Process
+	G4Decay* theDecayProcess = new G4Decay();
+	theParticleIterator->reset();
+	while( (*theParticleIterator)() ){
+		G4ParticleDefinition* particle = theParticleIterator->value();
+		G4ProcessManager* pmanager = particle->GetProcessManager();
+		if (theDecayProcess->IsApplicable(*particle)) {
+			pmanager ->AddProcess(theDecayProcess);
+			// set ordering for PostStepDoIt and AtRestDoIt
+			pmanager ->SetProcessOrdering(theDecayProcess, idxPostStep);
+			pmanager ->SetProcessOrdering(theDecayProcess, idxAtRest);
+		}
+	}
+}
+
+void MyPhysicsList::ConstructOp()
+{
+	G4Cerenkov* cerenkovProcess = new G4Cerenkov("Cerenkov");
+	cerenkovProcess->SetMaxNumPhotonsPerStep(fMaxNumPhotonStep);
+	cerenkovProcess->SetMaxBetaChangePerStep(10.0);
+	cerenkovProcess->SetTrackSecondariesFirst(true);
+	G4Scintillation* scintillationProcess = new G4Scintillation("Scintillation");
+	scintillationProcess->SetScintillationYieldFactor(1.);
+	scintillationProcess->SetScintillationExcitationRatio(0.0);
+	scintillationProcess->SetTrackSecondariesFirst(true);
+	G4OpAbsorption* absorptionProcess = new G4OpAbsorption();
+	G4RayleighScattering* rayleighScatteringProcess = new G4RayleighScattering();
+	rayleighScatteringProcess->SetEmModel(new G4PenelopeRayleighModel());
+	G4OpMieHG* mieHGScatteringProcess = new G4OpMieHG();
+	G4OpBoundaryProcess* boundaryProcess = new G4OpBoundaryProcess();
+
+	//	cerenkovProcess->SetVerboseLevel(fVerboseLebel);
+	//	scintillationProcess->SetVerboseLevel(fVerboseLebel);
+	//	absorptionProcess->SetVerboseLevel(fVerboseLebel);
+	//	rayleighScatteringProcess->SetVerboseLevel(fVerboseLebel);
+	//	mieHGScatteringProcess->SetVerboseLevel(fVerboseLebel);
+	//	boundaryProcess->SetVerboseLevel(fVerboseLebel);
+
+	// Use Birks Correction in the Scintillation process
+	if(!G4Threading::IsWorkerThread())
+	{
+		G4EmSaturation* emSaturation =
+				G4LossTableManager::Instance()->EmSaturation();
+		scintillationProcess->AddSaturation(emSaturation);
+	}
+
+	theParticleIterator->reset();
+	while( (*theParticleIterator)() ){
+		G4ParticleDefinition* particle = theParticleIterator->value();
+		G4ProcessManager* pmanager = particle->GetProcessManager();
+		G4String particleName = particle->GetParticleName();
+		if (cerenkovProcess->IsApplicable(*particle)) {
+			pmanager->AddProcess(cerenkovProcess);
+			pmanager->SetProcessOrdering(cerenkovProcess,idxPostStep);
+		}
+		if (scintillationProcess->IsApplicable(*particle)) {
+			pmanager->AddProcess(scintillationProcess);
+			pmanager->SetProcessOrderingToLast(scintillationProcess, idxAtRest);
+			pmanager->SetProcessOrderingToLast(scintillationProcess, idxPostStep);
+		}
+		if (particleName == "opticalphoton") {
+			G4cout << " AddDiscreteProcess to OpticalPhoton " << G4endl;
+			pmanager->AddDiscreteProcess(absorptionProcess);
+			pmanager->AddDiscreteProcess(rayleighScatteringProcess);
+			pmanager->AddDiscreteProcess(mieHGScatteringProcess);
+			pmanager->AddDiscreteProcess(boundaryProcess);
+		}
+	}
+}
+
+void MyPhysicsList::SetNbOfPhotonsCerenkov(G4int MaxNumber)
+{
+	fMaxNumPhotonStep = MaxNumber;
+}
+
+void MyPhysicsList::SetCuts()
+{
+	//  " G4VUserPhysicsList::SetCutsWithDefault" method sets
+	//   the default cut value for all particle types
+	//
+	// according to paper, set cut value
+	defaultCutValue = 0.5*um;
+	SetCutsWithDefault();
+
+	if (verboseLevel>0) DumpCutValuesTable();
+}
+
--- my_tests/SimpleDet/src/PhysicsList.hh
+++ my_tests/SimpleDet/src/PhysicsList.hh
@@ -1,22 +1,80 @@
 #ifndef PHYSICSLIST_HH
 #define PHYSICSLIST_HH
 
-#include "G4PhotoElectricEffect.hh"
+#include "globals.hh"
+
+#include "G4ParticleDefinition.hh"
+#include "G4ParticleTypes.hh"
+#include "G4ParticleTable.hh"
+
+#include "G4BosonConstructor.hh"
+#include "G4LeptonConstructor.hh"
+#include "G4MesonConstructor.hh"
+#include "G4BaryonConstructor.hh"
+#include "G4IonConstructor.hh"
+#include "G4ShortLivedConstructor.hh"
+
+
 #include "G4ComptonScattering.hh"
-#include "G4GammaConversion.hh"
-#include "G4Scintillation.hh"
 #include "G4PenelopeComptonModel.hh"
-#include "G4RayleighScattering.hh"
-#include "G4PenelopeRayleighModel.hh"
+#include "G4GammaConversion.hh"
+#include "G4PhotoElectricEffect.hh"
 #include "G4PenelopePhotoElectricModel.hh"
+
+#include "G4eMultipleScattering.hh"
+#include "G4MuMultipleScattering.hh"
+#include "G4hMultipleScattering.hh"
+
+#include "G4eIonisation.hh"
+#include "G4eBremsstrahlung.hh"
+#include "G4eplusAnnihilation.hh"
+
+#include "G4MuIonisation.hh"
+#include "G4MuBremsstrahlung.hh"
+#include "G4MuPairProduction.hh"
+#include "G4MuonMinusCapture.hh"
+
+#include "G4hIonisation.hh"
+
+#include "G4ProcessManager.hh"
+
+#include "G4Cerenkov.hh"
+#include "G4Scintillation.hh"
+#include "G4OpAbsorption.hh"
+
+#include "G4PenelopeRayleighModel.hh"
+#include "G4RayleighScattering.hh"
+#include "G4OpRayleigh.hh"
+
+#include "G4OpMieHG.hh"
+#include "G4OpBoundaryProcess.hh"
+
+#include "G4LossTableManager.hh"
+#include "G4EmSaturation.hh"
+
+#include "G4Threading.hh"
+#include "G4SystemOfUnits.hh"
+
+#include "G4Decay.hh"
 
 #include "QBBC.hh"
 
 class MyPhysicsList : G4VUserPhysicsList
 {
-	void ConstructEM();
+	G4int fMaxNumPhotonStep = 100;
 public:
-	void ConstructProcess();
+	//these methods Construct physics processes and register them
+	void ConstructEM();
+	// Optical Physics
+	void ConstructOp();
+	void ConstructDecay();
+
+	virtual void SetCuts();
+
+	virtual void ConstructProcess();
+	virtual void ConstructParticle();
+
+	void SetNbOfPhotonsCerenkov(G4int);
 };
 
 #endif
